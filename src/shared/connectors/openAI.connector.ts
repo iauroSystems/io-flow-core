ceabebc9b76b4ee4fea6036e1c2bc2a8e0bd1613
Sudhir Raut <135133833+sudhir-raut@users.noreply.github.com>
2023-09-11 12:50:30 +0530

    Packages upgrade (#24)
    * Open source launch commit
    
    * Update custom-messages.ts
    
    * Update package.json, LICENSE.md
    
    * Add value reference in properties array
    
    * Add 30009 as default port for REST and 40009 for gRPC
    
    * Upgrade npm packages
    
    ---------
    
    Co-authored-by: Sudhir Raut <sudhirraut@Sudhirs-MacBook-Air.local>

--- src/shared/connectors/openAI.connector.ts
+++ src/shared/connectors/openAI.connector.ts
@@ -1,32 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { OpenAIConnectorDto } from '../dtos/connectors.dto';
-import { Configuration, OpenAIApi } from 'openai';
+import { OpenAI } from 'openai';
 
 @Injectable()
 export class OpenAIConnector {
-  private openai: OpenAIApi;
 
   async call(openAIConnectorConfig: OpenAIConnectorDto): Promise<any> {
     try {
-      const configuration = new Configuration({
-        apiKey: openAIConnectorConfig.apiKey,
-      });
-      this.openai = new OpenAIApi(configuration);
+
+      const openai = new OpenAI({ apiKey: openAIConnectorConfig.apiKey, });
       const prompt = openAIConnectorConfig.prompt;
-      const responsePromise = this.openai.createCompletion(
+      const responsePromise = openai.chat.completions.create(
         {
           model: 'gpt-3.5-turbo',
-          prompt: prompt,
-          // messages: [{role: "user", content: prompt}],
+          messages: [{ role: "user", content: prompt }],
         },
         {
           timeout: 10000,
         },
       );
-        const response = await responsePromise;
-        return [null,response];
+      const response = await responsePromise;
+      return [null, response];
     } catch (error) {
-        return [error.toString(),null];
+      return [error.toString(), null];
     }
   }
 }
